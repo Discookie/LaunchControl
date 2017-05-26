21f5b2c649aa460f1abf7fa8318c47e6d0ecdfe1
Matekos Seventeen <Matekos Seventeen>
2017-05-26 21:50:18 +0200

    Initial commit

---
+++ rainbow.js
@@ -0,0 +1,137 @@
+var midiConnector = require('midi-launchpad').connect(0, 1, false);
+var launchpad;
+var mstates = {
+    "-1": 0,
+    0: 1,
+    1: 1,
+    2: 2,
+    3: 2,
+    4: 3,
+    5: 3,
+    6: 19,
+    7: 19,
+    8: 35,
+    9: 35,
+    10: 51,
+    11: 51,
+    12: 50,
+    13: 50,
+    14: 49,
+    15: 49,
+    16: 48,
+    17: 48,
+    18: 32,
+    19: 32,
+    20: 16,
+    21: 16,
+    22: 0,
+    23: 0
+}
+var states = {};
+var ml = 24;
+var ix = 2;
+var int = function() {
+    ix = 3;
+    states = {};
+    for (var i = 0; i < 8; i++) {
+        var stateInner = {};
+        for (var j = 0; j < 8; j++) {
+            stateInner[j] = (i + j) % ml -16;
+        }
+        states[i] = stateInner;
+    }
+}
+
+var restartable = true;
+var running = false;
+var intId = 0;
+console.log("Init");
+console.log(running==true?"true":"false");
+
+midiConnector.on("ready",function(lp) {
+    var update = function(){
+        for (var i = 0; i < 8; i++) {
+            var lm = "";
+            var col = (i * 3 + ix) % 5;
+            for (var j = 0; j < col; j++) lm += ".";
+            lm += "O";
+            var butt1 = launchpad.getButton(7-i, col);
+
+            states[i][col] = states[i][col]+1;
+
+            if (running || (states[7][7] < ml-2))
+                states[i][col] %= ml;
+            else
+                states[i][col] = Math.min(states[i][col], ml-2);
+
+            butt1.light(mstates[Math.max(states[i][col], -1)]);
+
+            //sleep.msleep(1);
+
+            if (col < 3) {
+
+                var prCol = col;
+                col += 5;
+                for (var j = prCol + 1; j < col; j++) lm += ".";
+                lm += "O";
+                var butt2 = launchpad.getButton(7-i, col);
+
+                states[i][col] = states[i][col]+1;
+
+                if (running || (states[7][7] < ml-2))
+                    states[i][col] %= ml;
+                else
+                    states[i][col] = Math.min(states[i][col], ml-2);
+
+                butt2.light(mstates[Math.max(states[i][col], -1)]);
+
+                //sleep.msleep(1);
+            }
+
+            for (j = col+1; j < 8; j++) lm += ".";
+            //console.log(lm);
+        }
+        if (!running) {
+            var rme = 64;
+
+            for (var i = 0; i < 8; i++) {
+                for (var j = 0; j < 8; j++) {
+                    if (states[i][j] >= ml-2 || states[i][j] < 0) {
+                        --rme;
+                    }
+                }
+            }
+            //console.log(rme);
+            if (rme <= 0) {
+                restartable = true;
+                clearInterval(intId);
+                console.log("Shut down fully.")
+            }
+
+        }
+        ix += 1;
+    }
+    lp.on("press", function(btn) {
+        //console.log("running:" + (running==true?"true":"false") + ", intId = " + intId);
+        if (running) {
+            restartable = false;
+            running = false;
+            console.log("Shutdown");
+            //console.log(running==true?"true":"false");
+        } else if (restartable) {
+            restartable = false;
+            running = true;
+            console.log("Restart");
+            //console.log(running==true?"true":"false");
+            int();
+            intId = setInterval(update, 100);
+        }
+    });
+    restartable = false;
+    running = true;
+    console.log("Start");
+    //console.log(running==true?"true":"false");
+    launchpad = lp;
+    int();
+    intId = setInterval(update, 100);
+});
